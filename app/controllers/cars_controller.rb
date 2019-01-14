19e02b475f409c9e0bdd9145c7944bb71f9f1fa8
Bennimaru <bennywhyu@gamil.com>
2019-01-14 15:23:35 -0500

    finished second drive

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,15 +1,23 @@
 class CarsController < ApplicationController
 
   def index
+    @cars = Car.all
+    render :index
   end
 
   def new
+    @car = Car.new
+    render :new
   end
-  
+
   def create
+    @car = Car.create(strong_params(:brand,:color,:model))
+    redirect_to car_path(@car)
   end
 
   def show
+    @car = Car.find(params[:id])
+    render :show
   end
 
   def edit
@@ -19,6 +27,12 @@ class CarsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def strong_params(*attributes)
+    params.require(:car).permit(*attributes)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_14_184849) do
+ActiveRecord::Schema.define(version: 2019_01_14_185504) do
 
   create_table "cars", force: :cascade do |t|
     t.string "brand"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 2019_01_14_184849) do
     t.string "model"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "owners", force: :cascade do |t|
+    t.string "name"
   end
 
 end
